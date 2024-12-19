890abd66536842b9fc06368479ee8136cf387376
a6iyyu <ffst4irs@gmail.com>
2024-12-19 10:44:56 +0700

    Temporary changes.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,13 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
+  // Hamburger Menu
   const HamburgerMenu = document.getElementById("hamburger-menu");
-
   HamburgerMenu.addEventListener("click", () => {});
+
+  // Keuntungan Modal Effect On Click
+  const KeuntunganModal = document.querySelectorAll("#keuntungan-modal");
+  KeuntunganModal.forEach((modal) => {
+    modal.addEventListener("mouseover", () => {
+      modal.style.transform = "scale(1.1)";
+    });
+  });
 });
